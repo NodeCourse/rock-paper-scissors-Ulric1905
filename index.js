281692cfdc3a8ff941c765c564092ae61dedcda0
unknown <emeric.lesault@ynov.com>
2018-05-15 09:23:40 +0200

    programe fonctionel

--- index.js
+++ index.js
@@ -15,45 +15,28 @@ app.get('/', (req, res) => {
     res.render('homepage');
 
 })
+
 app.get('/rock', (req, res) => {
-    res.render('rock');
-rdm = random();
-if (rdm == 'rock'){
-    console.log("fail")
-}
-else if (rdm == 'paper'){
-    console.log("succes")
-}
-else if (rdm == 'scissors'){
-    console.log("nop")
-}
+    resultat = isResult('rock');
+    res.render('rock', {
+        "status":resultat});
+
+
 })
 app.get('/paper', (req, res) => {
-    res.render('paper');
-    console.log("ok");
-rdm = random();
-if (rdm == 'rock'){
-    console.log("fail")
-}
-else if (rdm == 'paper'){
-    console.log("succes")
-}
-else if (rdm == 'scissors'){
-    console.log("nop")
-}
+    resultat = isResult('paper');
+    res.render('paper', {
+        "status":resultat});
+
+
 })
 app.get('/scissors', (req, res) => {
-    res.render('scissors');
-    rdm = random();
-    if (rdm == 'rock'){
-    console.log("fail")
-}
-    else if (rdm == 'paper'){
-    console.log("succes")
-}
-    else if (rdm == 'scissors'){
-        console.log("nop")
-    }
+    resultat = isResult('scissors');
+    res.render('scissors', {
+        "status":resultat});
+
+
+
 })
 
 function random() {
@@ -61,6 +44,64 @@ function random() {
     var result = randomItem(['rock', 'paper', 'scissors']);
     console.log(randomItem)
     return(result);
+}
+
+function isResult(Choice) {
+    rdm = random();
+    if (Choice == 'rock'){
+        if (rdm == 'rock'){
+            console.log("nop")
+            return ('equal')
+        }
+        else if (rdm == 'paper'){
+            console.log("fail")
+            return ('fail')
+
+        }
+        else if (rdm == 'scissors'){
+            console.log("succes")
+            return ('victory')
+
+        }
+
+    }
+    else if (Choice == 'paper'){
+        if (rdm == 'rock'){
+            console.log("succes")
+            return ('victory')
+
+        }
+        else if (rdm == 'paper'){
+            console.log("nop")
+            return ('equal')
+
+        }
+        else if (rdm == 'scissors'){
+            console.log("fail")
+            return ('fail')
+
+        }
+
+    }
+    else if (Choice == 'scissors'){
+        if (rdm == 'rock'){
+            console.log("fail")
+            return ('fail')
+
+        }
+        else if (rdm == 'paper'){
+            console.log("succes")
+            return ('victory')
+
+        }
+        else if (rdm == 'scissors'){
+            console.log("nop")
+            return ('equal')
+
+        }
+
+    }
+
 }
 
 app.listen(3000);
